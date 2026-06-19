640fcc8e93f603cc3087d223de5d84282ddfdb66
Kamil Mustafayev <github-email-kamil.mstfyv@gmail.com>
2026-06-19 12:16:19 +0400

    _id changed as id

--- todo-back/models/todo.js
+++ todo-back/models/todo.js
@@ -23,5 +23,12 @@ const todoSchema = new mongoose.Schema({
   isDone: { type: Boolean, default: false },
   date: { type: Date, default: Date.now },
 });
+todoSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 module.exports = mongoose.model("Todo", todoSchema);
